b89bad1d217426dbf2ad707b68946794ff438ec3
Annie Shin <annieyshin@gmail.com>
2018-10-30 16:47:16 -0700

    made into help queue

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+
+  ReactDOM.render(
+  <div>
+    <h1>Help Queue</h1>
+    <h3>3a</h3>
+    <h3>Thato and Haley</h3>
+    <p><em>Firebase will not save record!</em></p>
+  </div>,
+  document.getElementById('react-app-root')
+  );
